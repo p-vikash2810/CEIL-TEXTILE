b9e1fc5d816e7cbbb520e96c9192fc07a5c388df
vikash.patel1 <p.vikash2810@gmail.com>
2021-07-08 18:46:09 +0530

    card grid alignment corrected

--- src/pages/CardPage/CardPage.styles.js
+++ src/pages/CardPage/CardPage.styles.js
@@ -7,18 +7,37 @@ export const CardPageContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   align-items: center;
-  justify-content: space-evenly;
-  background-image: url(${bgImg});
+  justify-content: center;
+  /* background-image: url(${bgImg}); */
+  background: rgb(254, 193, 130);
+  background: linear-gradient(
+    50deg,
+    rgba(254, 193, 130, 1) 20%,
+    rgba(254, 160, 142, 1) 100%
+  );
   background-size: cover;
+  padding: 0 auto;
 `;
 
 export const CardPageWrapper = styled.div`
-  width: 100%;
+  width: auto;
   height: auto;
   display: flex;
   flex-wrap: wrap;
   align-items: center;
   justify-content: space-evenly;
-  backdrop-filter: blur(5px);
+  backdrop-filter: blur(10px);
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+  padding: 0 auto;
 
+  @media screen and (max-width: 960px) {
+    grid-template-columns: repeat(2, 1fr);
+
+  }
+  @media screen and (max-width: 600px) {
+    grid-template-columns: repeat(1, 1fr);
+
+  }
 `;
